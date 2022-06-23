3613c314c2c0752c8d8a72cd40b1d449c898f4ff
Leandro Javier Vallejos Lugo <LV_E91234@redlink.com.ar>
2022-06-23 15:54:03 -0300

    .

--- src/main/java/curso_link_2020/tp_link/Repo2Usuarios.java
+++ src/main/java/curso_link_2020/tp_link/Repo2Usuarios.java
@@ -1,9 +1,12 @@
 package curso_link_2020.tp_link;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "usuaries")
 public interface Repo2Usuarios extends PagingAndSortingRepository<Usuario, Integer> {
 
+	List<Usuario> findAll();
 }
